5db992b3ca53c1186e4c61450988a5b5b3e464e4
Gianluca Corrado <corrag@re-eduroam-195-169-168-235.wireless.uva.nl>
2016-02-25 13:10:15 +0100

    removed duplicate directory

--- rnacommender/pfam_util/__init__.py
+++ rnacommender/pfam_util/__init__.py
@@ -10,7 +10,7 @@ __status__ = "Production"
 def scan(seq):
     raise NotImplementedError("Implement wrapper for pfam_scan.pl")
 
-def dowload_seed_seqs(acc):
+def _dowload_seed_seqs(acc):
 
     def stockholm2fasta(stockholm):
         fasta = ""
@@ -37,3 +37,6 @@ def dowload_seed_seqs(acc):
         socket.close()
         fasta = stockholm2fasta(stockholm)
         return fasta
+
+def download_seeds(dom_list,temp_folder,verbose):
+
