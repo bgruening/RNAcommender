70a113e09d3d42d2654216f4580c05607fc01223
Gianluca Corrado <corrag@gmail.com>
2016-02-25 17:51:10 +0100

    Added parsing of xml results of sequence scan

--- rnacommender/pfam_utils/__init__.py
+++ rnacommender/pfam_utils/__init__.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from urllib2 import urlopen, URLError
+import xml.etree.ElementTree as ET
+from math import ceil
 
 __author__ = "Gianluca Corrado"
 __copyright__ = "Copyright 2016, Gianluca Corrado"
@@ -8,13 +10,49 @@ __maintainer__ = "Gianluca Corrado"
 __email__ = "gianluca.corrado@unitn.it"
 __status__ = "Production"
 
-def scan(seq):
-    raise NotImplementedError("Implement wrapper for pfam_scan.pl")
+def sequence_search(seq_id,seq,header=False):
+
+    def add_spaces(text,mul=8):
+        l = len(text)
+        next_mul = int(ceil(l / mul)+1) * mul
+        offset = next_mul - l
+        if offset == 0:
+            offset = 8
+        return text + " "*offset
+
+    result_url = "http://pfam.xfam.org/search/sequence/resultset/D6C37D2C-DBD5-11E5-8641-F81495374F7B?output=xml"
+    socket = urlopen(result_url)
+    result_xml = socket.read()
+    socket.close()
+    root = ET.fromstring(result_xml)
+    matches = root[0][0][0][0][:]
+    if header:
+        ret = "<seq id>        <alignment start>       <alignment end> <envelope start>        <envelope end>  <hmm acc>       <hmm name>      <type>  <hmm start>     <hmm end>       <hmm length>    <bit score>     <E-value>       <significance>  <clan>\n"
+    else:
+        ret = ""
+    for match in matches:
+        for location in match:
+            ret += add_spaces(seq_id)
+            ret += add_spaces(location.attrib['ali_start'])
+            ret += add_spaces(location.attrib['ali_end'])
+            ret += add_spaces(location.attrib['start'])
+            ret += add_spaces(location.attrib['end'])
+            ret += add_spaces(match.attrib['accession'])
+            ret += add_spaces(match.attrib['id'])
+            ret += add_spaces(match.attrib['class'])
+            ret += add_spaces(location.attrib['hmm_start'])
+            ret += add_spaces(location.attrib['hmm_end'])
+            ret += add_spaces("None")
+            ret += add_spaces(location.attrib['bitscore'])
+            ret += add_spaces(location.attrib['evalue'])
+            ret += add_spaces(location.attrib['significant'])
+            ret += "None\n"
+    return ret
 
 def read_pfam_output(pfam_out_file):
     cols = ["seq_id","alignment_start","alignment_end","envelope_start","envelope_end","hmm_acc","hmm_name","type","hmm_start","hmm_end","hmm_length","bit_score","E-value","significance","clan"]
     data = pd.read_table(pfam_out_file,
-            sep="\s*",skip_blank_lines=True,skiprows=1,names=cols)
+            sep="\s*",skip_blank_lines=True,skiprows=1,names=cols,engine='python')
     return data
 
 def download_seed_seqs(acc):
