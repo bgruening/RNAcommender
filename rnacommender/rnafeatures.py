55b09ca4b7fb69f2adb01b53fe067c7cad3f2f6d
Gianluca Corrado <corrag@gmail.com>
2016-03-10 09:51:20 +0100

    Initial commit for RNA features script

---
+++ rnacommender/rnafeatures.py
@@ -0,0 +1,8 @@
+
+
+__author__ = "Gianluca Corrado"
+__copyright__ = "Copyright 2016, Gianluca Corrado"
+__license__ = "MIT"
+__maintainer__ = "Gianluca Corrado"
+__email__ = "gianluca.corrado@unitn.it"
+__status__ = "Production"
