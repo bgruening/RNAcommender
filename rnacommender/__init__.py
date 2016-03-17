48765169b0fd6e75a2075abcc394665180f9c73e
Gianluca Corrado <corrag@gmail.com>
2016-03-17 10:04:18 +0100

    added __init__

---
+++ rnacommender/__init__.py
@@ -0,0 +1,6 @@
+__author__ = "Gianluca Corrado"
+__copyright__ = "Copyright 2016, Gianluca Corrado"
+__license__ = "MIT"
+__maintainer__ = "Gianluca Corrado"
+__email__ = "gianluca.corrado@unitn.it"
+__status__ = "Production"
