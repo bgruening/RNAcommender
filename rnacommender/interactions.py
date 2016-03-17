1107bfc13b8944b55ac57056d6472d27a26e0729
Gianluca Corrado <corrag@gmail.com>
2016-03-17 10:19:24 +0100

    flake8 compliant

--- rnacommender/interactions.py
+++ rnacommender/interactions.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-import sys
+
 import argparse
+import sys
+
 import numpy as np
+
 import pandas as pd
+
 
 __author__ = "Gianluca Corrado"
 __copyright__ = "Copyright 2016, Gianluca Corrado"
@@ -13,9 +17,12 @@ __maintainer__ = "Gianluca Corrado"
 __email__ = "gianluca.corrado@unitn.it"
 __status__ = "Production"
 
+
 class InteractionMatrix():
     """Prepare interaction matrix from interaction list"""
-    def __init__(self,interaction_list,output,low_throughput_rbps=None,verbose=True):
+
+    def __init__(self, interaction_list, output, low_throughput_rbps=None,
+                 verbose=True):
         """
         Params
         ------
@@ -25,7 +32,8 @@ class InteractionMatrix():
 
         output : str
             Name of the output file. The output file is an HDF5 containing a
-            pandas DataFrame inside this file has proteins as columns and RNAs as rows (index).
+            pandas DataFrame inside this file has proteins as columns and RNAs
+            as rows (index).
 
         low_throughput_rbps : str (default : None)
             File containing a list (new line separated) of RBPs
@@ -48,8 +56,9 @@ class InteractionMatrix():
     def prepare(self):
         if self.verbose:
             print("Preparing interaction matrix for", end=' ')
-        #read interaction list
-        data = pd.read_table(self.interaction_list,sep='\t',comment='#',header=None)
+        # read interaction list
+        data = pd.read_table(self.interaction_list,
+                             sep='\t', comment='#', header=None)
         # determine the names of the RBPs and the RNAs
         rbps = data[0].unique()
         rnas = data[1].unique()
@@ -58,20 +67,20 @@ class InteractionMatrix():
         m = rnas.shape[0]
 
         if self.verbose:
-            print("%i proteins and %i RNAs..." % (n,m), end=' ')
+            print("%i proteins and %i RNAs..." % (n, m), end=' ')
 
-        Y = pd.DataFrame(np.zeros((m,n)),columns=rbps,index=rnas)
+        y = pd.DataFrame(np.zeros((m, n)), columns=rbps, index=rnas)
         # Set NaN the interactions for the low-throughput RBPs
         for rbp in self.low_throughput_rbps:
-            Y[rbp][:] = np.nan
+            y[rbp][:] = np.nan
         # scan the data and update the interaction matrix
         for e in data.iterrows():
             rbp = e[1][0]
             rna = e[1][1]
-            Y[rbp][rna] = 1
+            y[rbp][rna] = 1
         # save the DataFrame to HDF5
         store = pd.io.pytables.HDFStore(self.output)
-        store["matrix"] = Y
+        store["matrix"] = y
         store.close()
 
         if self.verbose:
@@ -80,20 +89,28 @@ class InteractionMatrix():
             sys.stdout.flush()
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description=__doc__,
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('interaction_list', metavar='interaction_list', type=str,
-                        help="""File containing the interaction list. First column: proteins, second column: rnas (tab separated).""")
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('interaction_list', metavar='interaction_list',
+                        type=str, help="""File containing the interaction list.\
+                        First column: proteins, second column: rnas\
+                        (tab separated).""")
     parser.add_argument('output', metavar='output', type=str,
-                        help="""File name of the HDF Store to save the intereaction matrix.""")
-    parser.add_argument('--low', metavar='low_throughput_rbps', type=str, default=None,
-                        help="""File containing a list (new line separated) of RBPs with low-throughput evidence only. Their missing interactions will be labeled with NaN.""")
-    parser.add_argument('--quiet', dest='quiet', action='store_true', default=False,
-                        help="""Do not print information at STDOUT.""")
+                        help="""File name of the HDF Store to save the \
+                        intereaction matrix.""")
+    parser.add_argument('--low', metavar='low_throughput_rbps', type=str,
+                        default=None, help="""File containing a list (new line \
+                        separated) of RBPs with low-throughput evidence only. \
+                        Their missing interactions will be labeled with \
+                        NaN.""")
+    parser.add_argument('--quiet', dest='quiet', action='store_true',
+                        default=False, help="""Do not print information \
+                        at STDOUT.""")
 
     args = parser.parse_args()
 
     im = InteractionMatrix(interaction_list=args.interaction_list,
-                        output=args.output,
-                        verbose=(not args.quiet))
+                           output=args.output,
+                           verbose=(not args.quiet))
     im.prepare()
